2e4c65b56fad60bbb6c2daf1f4b9e383f25cc3ef
Jacksonmills <jacksonemills@gmail.com>
2023-10-22 02:18:28 -0500

    Add ClerkProvider for authentication and updateUser function in middleware.
    - Added ClerkProvider for authentication.
    - Created a new middleware file.
    - Added authMiddleware function from Clerk.
    - Configured public routes to include '/'.
    
    This commit implements authentication using ClerkProvider and adds a new middleware file with authMiddleware function from Clerk to protect routes. The public routes configuration includes '/' to allow public access.

--- src/app/_components/providers.tsx
+++ src/app/_components/providers.tsx
@@ -2,18 +2,21 @@ import React from 'react';
 import { ThemeProvider } from './ThemeProvider';
 import { TRPCReactProvider } from '@/trpc/react';
 import { headers } from 'next/headers';
+import { ClerkProvider } from '@clerk/nextjs';
 
 export default function Providers({ children }: { children: React.ReactNode; }) {
   return (
-    <TRPCReactProvider headers={headers()}>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        {children}
-      </ThemeProvider>
-    </TRPCReactProvider>
+    <ClerkProvider>
+      <TRPCReactProvider headers={headers()}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </TRPCReactProvider>
+    </ClerkProvider>
   );
 }
